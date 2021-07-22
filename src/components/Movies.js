7aeae2b9e28d8fa9646cca470906895b387f610d
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-22 17:13:35 -0400

    finished

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const listOfMovies = movies.map(movie => (
+    <div key={movie.title}>
+      <h2>
+        {movie.title}
+      </h2>
+      <p>
+        Run time: {movie.time} minutes
+      </p>
+      <ul>
+        {movie.genres.map(genre => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+
+    </div>
+  ))
+  return (
+  <div>
+    <h1>Movies Page</h1>
+       {listOfMovies}
+    </div>
+  )
 }
 
 export default Movies;
